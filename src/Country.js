8b6e5673a74f66e45d0c3d639fe82b3f915538aa
alamin <alamin.nava@gmail.com>
2022-11-14 17:57:44 +0600

    remove country

--- src/Country.js
+++ src/Country.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 export default function Country(props) {
     const country = props.country
+    const handleRemove = (name) =>{
+        props.onData(name)
+    }
     return (
         <div className='col-3 mb-4'>
             <div className="card">
-                <div className='country-flag-wrap'><img src={country.flags.png} className="card-img-top h-100" alt="..." /></div>
+                <div className='country-flag-wrap'><img src={country.flags.png} className="card-img-top h-100" alt={country.name.common} /></div>
                 <div className="card-body">
                     <h6 className="card-title">Name: {country.name.common}</h6>
-                    <h6>Population: <span>{country.population}</span> </h6>
-                    <h6>Capital: {country.capital}</h6>
-                    <h6>Continents: {country.continents}</h6>
+                    <h6>Population: <span className='fw-normal'>{country.population}</span> </h6>
+                    <h6>Capital: <span className='fw-normal'>{country.capital}</span></h6>
+                    <h6>Continents: <span className='fw-normal'>{country.continents}</span></h6>
+                    <button className='btn btn-sm btn-danger' onClick={()=>{handleRemove(country.name.common)}}>Remove Country</button>
                 </div>
             </div>
         </div>
